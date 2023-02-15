d2b4ee85648403d17bcf0cba4715d6f0a818d1bd
Alexey Raptev <raptew@gmail.com>
2023-02-15 20:30:12 +0700

    add console app

---
+++ MLConsole/ImagePrediction.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+using Microsoft.ML.Data;
+using Tensorflow;
+
+namespace MLConsole;
+
+public class ImagePrediction : ImageData
+{
+    public float[] Score;
+
+    public string PredictedLabelValue;
+}
